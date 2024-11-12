342f646a902e9cf48a416fa0cc72d74c05824736
FedericoChiodi <federico.chiodi@edu.unife.it>
2024-11-12 13:41:08 +0100

    Refactor

--- app/src/main/java/com/sanpc/roadsense/ui/screen/Screen.kt
+++
@@ -1,8 +0,0 @@
-package com.sanpc.roadsense.ui.screen
-
-sealed class Screen (val screen: String){
-    data object Home: Screen("Home")
-    data object Profile: Screen("Profile")
-    data object Map: Screen("Map")
-    data object Reports: Screen("Reports")
-}
