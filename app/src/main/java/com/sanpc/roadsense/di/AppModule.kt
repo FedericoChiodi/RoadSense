17e415a1692629bbff959982ab7b73961302bbaf
FedericoChiodi <federico.chiodi@edu.unife.it>
2024-11-12 12:10:56 +0100

    Added res, icon and refactor

--- app/src/main/java/com/sanpc/roadsense/di/AppModule.kt
+++ app/src/main/java/com/sanpc/roadsense/di/AppModule.kt
@@ -1,4 +1,39 @@
 package com.sanpc.roadsense.di
 
+import android.content.Context
+import androidx.room.Room
+import com.sanpc.roadsense.data.dao.DropDao
+import com.sanpc.roadsense.data.dao.PotholeDao
+import com.sanpc.roadsense.data.database.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext appContext: Context): AppDatabase {
+        return Room.databaseBuilder(
+            appContext,
+            AppDatabase::class.java,
+            "RoadSense_db"
+        ).build()
+    }
+
+    @Provides
+    fun provideDropDao(database: AppDatabase): DropDao {
+        return database.dropDao()
+    }
+
+    @Provides
+    fun providePotholeDao(database: AppDatabase): PotholeDao {
+        return database.potholeDao()
+    }
+
 }
